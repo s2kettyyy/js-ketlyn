d9d41d301667e72d55836a5ceea1fb5e0a8029ea
s2kettyyy <ketlyn.sincoski@escola.pr.gov.br>
2024-08-09 10:42:54 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,1 @@
-function setup() {
-  createCanvas(600, 600);
-}
 
-function draw() {
-  background("white");
-  
-  stroke("blue");
-  fill("red");
-  
-// console.log (mouseIsPressed);
-  
-  if(mouseIsPressed) {
-  rect(mouseX,mouseY,20,35);
-  }
-}
